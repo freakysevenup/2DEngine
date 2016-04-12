5a205505b0a5c3bd15711eb8c1252def165eb4ad
Jesse Derochie <jesse.a.derochie@gmail.com>
2016-04-11 20:45:39 -0400

    Sprites
    *SpriteBatch
    *Sprite

--- GameEngine2D/Core/SpriteBatch.h
+++ GameEngine2D/Core/SpriteBatch.h
@@ -5,13 +5,41 @@
 #include <glm\glm.hpp>
 using namespace glm;
 
+enum VBO
+{
+	POSITION,
+	COLOUR,
+	TEXCOORDS,
+	NUM_VBOS
+};
+
+enum GlyphSort
+{
+	NONE,
+	FRONT_TO_BACK,
+	BACK_TO_FRONT,
+	TEXTURE
+};
+
 class Glyph
 {
 public:
 
 	Glyph() {}
-	Glyph(const vec4& destRect, const vec4& uvRect, GLuint TextureID, float zLayer, const vec4&colour);
-	Glyph(const vec4& destRect, const vec4& uvRect, GLuint TextureID, float zLayer, const vec4&colour, float angle);
+	Glyph(
+		const vec4& destRect, 
+		const vec4& uvRect, 
+		GLuint TextureID, 
+		float zLayer, 
+		const vec4&colour);
+
+	Glyph(
+		const vec4& destRect, 
+		const vec4& uvRect, 
+		GLuint TextureID, 
+		float zLayer, 
+		const vec4&colour, 
+		float angle);
 
 	Vertex topLeft_;
 	Vertex bottomLeft_;
@@ -19,7 +47,7 @@ public:
 	Vertex bottomRight_;
 
 	GLuint texture_;
-	float depth_;
+	float zLayer_;
 	float angle_;
 
 private:
@@ -31,7 +59,10 @@ class RenderBatch
 {
 public:
 
-	RenderBatch(GLuint offSet, GLuint numVertices, GLuint textureID) :
+	RenderBatch(
+		GLuint offSet, 
+		GLuint numVertices, 
+		GLuint textureID) :
 		offSet_(offSet),
 		numVertices_(numVertices),
 		textureID_(textureID) { }
@@ -44,6 +75,50 @@ class SpriteBatch
 {
 public:
 	SpriteBatch();
-	~SpriteBatch();
+	~SpriteBatch() { }
+
+	void Begin(GlyphSort sortType = GlyphSort::TEXTURE);
+	void End();
+
+	void Draw(
+		const vec4& destRect, 
+		const vec4& uvRect, 
+		GLuint textureID, 
+		float zLayer, 
+		const vec4& colour);
+
+	void Draw(
+		const vec4& destRect, 
+		const vec4& uvRect, 
+		GLuint textureID, 
+		float zLayer, 
+		const vec4& colour, 
+		float angle);
+
+	void Draw(
+		const vec4& destRect, 
+		const vec4& uvRect, 
+		GLuint textureID, 
+		float zLayer, 
+		const vec4& colour, 
+		const vec2& direction);
+
+	void Render();
+
+private:
+
+	void CreateRenderBatches();
+	void SortGlyphs();
+
+	static bool CompareFrontToBack(Glyph * a, Glyph * b);
+	static bool CompareBackToFront(Glyph * a, Glyph * b);
+	static bool CompareTexture(Glyph * a, Glyph * b);
+
+	GLuint m_VBO[NUM_VBOS], m_VAO;
+	GlyphSort m_sortType;
+
+	std::vector<Glyph> m_glyphs;
+	std::vector<Glyph *> m_glyphPointers;
+	std::vector<RenderBatch> m_renderBatches;
 };
 
